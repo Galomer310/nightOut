9103b3c55bdb3781166a9a8e9874b489cb54fa55
Gal Omer <galomer6708@gmail.com>
2024-10-19 20:48:19 +0300

    round up to usual pleace calc

--- usualPlace.js
+++ usualPlace.js
@@ -13,14 +13,14 @@ document.getElementById("beer").addEventListener("click", function() {
     var beerCount = prompt("How many beers did you drink?");
     beerCount = Number(beerCount);
     beerPayment = beer * beerCount;
-    document.getElementById("beer").innerHTML = beerPayment;
+    document.getElementById("beer").innerHTML = beerPayment.toFixed(1);
     updateTotal(); 
 });
 document.getElementById("shot").addEventListener("click", function() {
     var caesarCount = prompt("How many caesars did you have?");
     caesarCount = Number(caesarCount);
     caesarPayment = caesar * caesarCount;
-    document.getElementById("shot").innerHTML = caesarPayment;
+    document.getElementById("shot").innerHTML = caesarPayment.toFixed(1);
     updateTotal();
 });
 // //////////////////////////////////////////////////////////////////////////////////////////
@@ -33,7 +33,7 @@ document.getElementById("food").addEventListener("click", function() {
     var participants = prompt("How many people shared the dish? If only you, press 1");
     participants = Number(participants);
     foodPayment = food / participants;
-    document.getElementById("food").innerHTML = foodPayment;
+    document.getElementById("food").innerHTML = foodPayment.toFixed(1);
     updateTotal();
 });
 
@@ -43,16 +43,17 @@ document.getElementById("tip").addEventListener("click", function() {
     var tipByPercentage = prompt("What percentage of tip would you like to leave? Type a number!");
     tipByPercentage = Number(tipByPercentage);
     var tipToAdd = (TotalCount * tipByPercentage) / 100; 
-    document.getElementById("tip").innerHTML = tipToAdd;
+    document.getElementById("tip").innerHTML = tipToAdd.toFixed(1);
 
     var needToPay = TotalCount + tipToAdd;
-    document.getElementById("Payment").innerHTML = needToPay; // Corrected ID selection
-});
+    document.getElementById("Payment").innerHTML = needToPay.toFixed(1); // Ensure it displays with one decimal place
+    });
 
 function updateTotal() {
     TotalCount = beerPayment + caesarPayment + foodPayment;
-    document.getElementById("Payment").innerHTML = TotalCount;
+    document.getElementById("Payment").innerHTML = TotalCount.toFixed(1);
 }
+
 
 
 
